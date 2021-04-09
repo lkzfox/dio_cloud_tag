592eb2be8c3038629717120d869d3efb2d1b44e8
Lucas Feitoza <jlkzfp@gmail.com>
2021-04-09 19:54:18 -0300

    Adicionada remoção de dependencias ao deletar

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,7 @@
 class Content < ApplicationRecord
     belongs_to :user, foreign_key: 'id_user'
 
-    has_many :tag_contents, foreign_key: :id_content
+    has_many :tag_contents, foreign_key: :id_content, dependent: :destroy
     has_many :tags, through: :tag_contents
 
     validates :title, :description, presence: true
