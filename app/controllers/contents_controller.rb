722393b4da291a5178f2758815dd7c6cfd344f9c
Lucas Feitoza <jlkzfp@gmail.com>
2021-04-12 19:17:04 -0300

    Adicionada tela de edição e exibição dos conteudos

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -20,6 +20,24 @@ class ContentsController < ApplicationController
     end
   end
 
+  def edit
+    @content = Content.find(params.require(:id))
+  end
+
+  def update
+    @content = Content.find(params[:id])
+
+    if @content.update(content_params)
+      redirect_to contents_path, notice: 'Success!'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @content = Content.find(params.require(:id))
+  end
+
   private 
 
   def content_params
